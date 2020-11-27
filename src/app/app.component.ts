bd083c8a7d66f6042aa3771da9c64218ce743a84
unknown <rajarshitiger@gmail.com>
2020-11-27 11:25:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Model } from './model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practise1';
+  signupForm:FormGroup;
+  ar:Model[]=[];
+  url:any = '';
+  file:any;
+  name1='';
+  url1:any='';
+  
+status=false;
+ngOnInit(){
+  this.signupForm=new FormGroup({
+    'name':new FormControl(null,Validators.required),
+    'url':new FormControl(null,Validators.required)
+  })
+}
+
+
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); 
+      this.file=event.target.files[0];
+
+      reader.onload = (event) => { 
+        this.url = event.target.result;
+      }
+    }
+  }
+  
+  onSubmit(){
+  this.ar.push({
+    name:this.signupForm.value.name,
+    url:this.url
+   
+  }
+
+  
+  
+);
+this.status=true;
+console.log(this.signupForm);
+this.name1=this.signupForm.value.name;
+this.url1=this.url;
+this.signupForm.reset();
+
+this.url='';
+  }
+
+
+onClear(){
+  this.signupForm.reset();
+  this.status=false;
+}
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { NgxDocViewerModule } from 'ngx-doc-viewer';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    PdfViewerModule,
+    NgxDocViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
